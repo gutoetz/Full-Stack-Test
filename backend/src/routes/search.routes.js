5990393564bb7c5e7009fc4d777d3dfbf1abed40
Augusto Etz <augustoetz@hotmail.com>
2023-04-13 21:57:38 -0300

    fix: deploy

--- backend/src/routes/search.routes.js
+++ backend/src/routes/search.routes.js
@@ -4,6 +4,7 @@ const searchControllers = require('../controllers/searchControllers');
 
 const router = express.Router()
 
+router.get(`/`, (req, res) => res.status(200).json('connected'))
 router.get(`/:brand/:category/search`, searchControllers.getAll)
 
 module.exports = router;
